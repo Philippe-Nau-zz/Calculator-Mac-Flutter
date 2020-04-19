211f2a0369637e8fd8baa62c40cf50d9657a5ce9
Philippenau <philippe.bnu@gmail.com>
2020-04-19 14:35:23 -0300

    Mac calculator finished

---
+++ lib/screens/calculator.dart
@@ -0,0 +1,50 @@
+import 'package:calculator/models/memory.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import '../components/display.dart';
+import '../components/keyboard.dart';
+
+class Calculator extends StatefulWidget {
+
+  @override
+  _CalculatorState createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  final Memory memory = Memory();
+
+  _onPressed(String command) {
+    setState(() {
+      memory.applyCommand(command);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp
+    ]);
+
+    return MaterialApp(
+      home: Column(
+        children: <Widget>[
+
+          Display(memory.value),
+          Keyboard(_onPressed),
+        ],
+      ),
+    );
+  }
+}
+
+void openPage(BuildContext context) {
+  Navigator.push(context, MaterialPageRoute(
+    builder: (BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Next page'),
+        ),
+      );
+    },
+  ));
+}
